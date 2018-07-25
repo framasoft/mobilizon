c4cfb081e79aa218d56e77ade3035c38bb06c601
Thomas Citharel <tcit@tcit.fr>
2018-07-25 16:42:04 +0200

    Add docker-compose dev
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

---
+++ js/docker/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+npm install
+npm rebuild node-sass
+npm run serve
--- priv/repo/migrations/20180109150000_prerequites.exs
+++ priv/repo/migrations/20180109150000_prerequites.exs
@@ -2,15 +2,18 @@ defmodule Eventos.Repo.Migrations.Prerequites do
   use Ecto.Migration
 
   def up do
-    execute """
+    execute("""
     CREATE TYPE datetimetz AS (
         dt timestamptz,
         tz varchar
     );
-    """
+    """)
+
+    execute("CREATE EXTENSION IF NOT EXISTS postgis")
   end
 
   def down do
-    execute "DROP TYPE IF EXISTS datetimetz;"
+    execute("DROP TYPE IF EXISTS datetimetz;")
+    execute("DROP EXTENSION IF EXISTS postgis")
   end
 end
