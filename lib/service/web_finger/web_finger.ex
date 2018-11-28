afb9b797ba0f48950a92f815fb9ea76fb36a674e
Thomas Citharel <tcit@tcit.fr>
2018-11-28 15:00:15 +0100

    Fix tests
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- lib/service/web_finger/web_finger.ex
+++ lib/service/web_finger/web_finger.ex
@@ -38,7 +38,7 @@ defmodule Mobilizon.Service.WebFinger do
       {:ok, represent_actor(actor, "JSON")}
     else
       _e ->
-        with %Actor{} = actor when not is_nil(actor) <- Actors.get_actor_by_url(resource) do
+        with {:ok, %Actor{} = actor} when not is_nil(actor) <- Actors.get_actor_by_url(resource) do
           {:ok, represent_actor(actor, "JSON")}
         else
           _e ->
