80c6351d2f9f32332383223b64d464e7afeb7fa8
Thomas Citharel <tcit@tcit.fr>
2019-01-15 14:22:06 +0100

    Remove type after column
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- priv/repo/migrations/20190114162055_remove_address_type.exs
+++ priv/repo/migrations/20190114162055_remove_address_type.exs
@@ -3,8 +3,8 @@ defmodule Mobilizon.Repo.Migrations.RemoveAddressType do
   require Logger
 
   def up do
-    execute "DROP TYPE IF EXISTS address_type"
     execute "ALTER TABLE \"events\" DROP COLUMN IF EXISTS address_type"
+    execute "DROP TYPE IF EXISTS address_type"
     rename table(:events), :phone, to: :phone_address
   end
 end
