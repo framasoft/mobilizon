1a4197f9773ec1ee52e2c756786d25b99a4bfb9a
Thomas Citharel <thomas.citharel@framasoft.org>
2019-02-08 11:35:35 +0100

    Update deps

--- .credo.exs
+++ .credo.exs
@@ -102,7 +102,6 @@
         {Credo.Check.Refactor.CyclomaticComplexity, []},
         {Credo.Check.Refactor.FunctionArity, []},
         {Credo.Check.Refactor.LongQuoteBlocks, []},
-        {Credo.Check.Refactor.MapInto, []},
         {Credo.Check.Refactor.MatchInCondition, []},
         {Credo.Check.Refactor.NegatedConditionsInUnless, []},
         {Credo.Check.Refactor.NegatedConditionsWithElse, []},
