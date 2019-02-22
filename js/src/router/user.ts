e19a533e9dbb25dbe1b929ccd726728550482551
Chocobozzz <me@florianbigard.com>
2019-02-22 11:24:41 +0100

    Add leave/join/delete event logic

--- js/src/router/user.ts
+++ js/src/router/user.ts
@@ -7,54 +7,54 @@ import SendPasswordReset from '@/views/User/SendPasswordReset.vue';
 import PasswordReset from '@/views/User/PasswordReset.vue';
 
 export default [
-    {
-        path: '/register/user',
-        name: 'Register',
-        component: RegisterUser,
-        props: true,
-        meta: { requiredAuth: false },
-    },
-    {
-        path: '/register/profile',
-        name: 'RegisterProfile',
-        component: RegisterProfile,
-        props: true,
-        meta: { requiredAuth: false },
-    },
-    {
-        path: '/resend-instructions',
-        name: 'ResendConfirmation',
-        component: ResendConfirmation,
-        props: true,
-        meta: { requiresAuth: false },
-    },
-    {
-        path: '/password-reset/send',
-        name: 'SendPasswordReset',
-        component: SendPasswordReset,
-        props: true,
-        meta: { requiresAuth: false },
-    },
-    {
-        path: '/password-reset/:token',
-        name: 'PasswordReset',
-        component: PasswordReset,
-        meta: { requiresAuth: false },
-        props: true,
-    },
-    {
-        path: '/validate/:token',
-        name: 'Validate',
-        component: Validate,
-        // We can only pass string values through params, therefore
-        props: (route) => ({ email: route.params.email, userAlreadyActivated: route.params.userAlreadyActivated === 'true'}),
-        meta: { requiresAuth: false },
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login,
-        props: true,
-        meta: { requiredAuth: false },
-    },
+  {
+    path: '/register/user',
+    name: 'Register',
+    component: RegisterUser,
+    props: true,
+    meta: { requiredAuth: false },
+  },
+  {
+    path: '/register/profile',
+    name: 'RegisterProfile',
+    component: RegisterProfile,
+    props: true,
+    meta: { requiredAuth: false },
+  },
+  {
+    path: '/resend-instructions',
+    name: 'ResendConfirmation',
+    component: ResendConfirmation,
+    props: true,
+    meta: { requiresAuth: false },
+  },
+  {
+    path: '/password-reset/send',
+    name: 'SendPasswordReset',
+    component: SendPasswordReset,
+    props: true,
+    meta: { requiresAuth: false },
+  },
+  {
+    path: '/password-reset/:token',
+    name: 'PasswordReset',
+    component: PasswordReset,
+    meta: { requiresAuth: false },
+    props: true,
+  },
+  {
+    path: '/validate/:token',
+    name: 'Validate',
+    component: Validate,
+    // We can only pass string values through params, therefore
+    props: (route) => ({ email: route.params.email, userAlreadyActivated: route.params.userAlreadyActivated === 'true'}),
+    meta: { requiresAuth: false },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    props: true,
+    meta: { requiredAuth: false },
+  },
 ];
