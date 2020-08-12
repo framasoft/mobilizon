3ca4291c9b381d09efc7581de2b80b5506f6da13
Thomas Citharel <tcit@tcit.fr>
2020-08-12 17:55:38 +0200

    Remove the /events/explore route
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- js/src/components/NavBar.vue
+++ js/src/components/NavBar.vue
@@ -6,7 +6,7 @@
       </b-navbar-item>
     </template>
     <template slot="start">
-      <b-navbar-item tag="router-link" :to="{ name: RouteName.EXPLORE }">{{
+      <b-navbar-item tag="router-link" :to="{ name: RouteName.SEARCH }">{{
         $t("Explore")
       }}</b-navbar-item>
       <b-navbar-item tag="router-link" :to="{ name: RouteName.MY_EVENTS }">{{
