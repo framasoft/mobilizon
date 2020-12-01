d6d930978466d139ab85900cde501f6cf4ae0329
Thomas Citharel <tcit@tcit.fr>
2020-12-01 09:10:11 +0100

    [LDAP] Allow to filter users by memberOf
    Closes #528
    
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- config/config.exs
+++ config/config.exs
@@ -146,6 +146,8 @@ config :mobilizon, :ldap,
   base: System.get_env("LDAP_BASE") || "dc=example,dc=com",
   uid: System.get_env("LDAP_UID") || "cn",
   require_bind_for_search: !(System.get_env("LDAP_REQUIRE_BIND_FOR_SEARCH") == "false"),
+  # The full CN to filter by `memberOf`, or `false` if disabled
+  group: false,
   # Either the admin UID matching the field in `uid`,
   # Either a tuple with the fully qualified DN: {:full, uid=admin,dc=example.com,dc=local}
   bind_uid: System.get_env("LDAP_BIND_UID"),
