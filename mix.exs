aeb6b2679c2887d0b67094f7ce4eaf398f6e283f
Thomas Citharel <tcit@tcit.fr>
2021-02-04 18:57:50 +0100

    Require Elixir 1.11
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Mobilizon.Mixfile do
     [
       app: :mobilizon,
       version: @version,
-      elixir: "~> 1.8",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       xref: [exclude: [:eldap]],
