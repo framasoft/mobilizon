bfb04bb84db6affae79587ef2a08a7579eef1c16
Thomas Citharel <tcit@tcit.fr>
2021-05-12 18:19:03 +0200

    Make deleting an actor ignoring error when deleting files
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- lib/mobilizon/actors/actors.ex
+++ lib/mobilizon/actors/actors.ex
@@ -1343,6 +1343,7 @@ defmodule Mobilizon.Actors do
 
   defp remove_banner(%Actor{banner: %File{url: url}} = actor) do
     safe_remove_file(url, actor)
+    {:ok, actor}
   end
 
   @spec remove_avatar(Actor.t()) :: {:ok, Actor.t()}
@@ -1350,6 +1351,7 @@ defmodule Mobilizon.Actors do
 
   defp remove_avatar(%Actor{avatar: %File{url: url}} = actor) do
     safe_remove_file(url, actor)
+    {:ok, actor}
   end
 
   @spec safe_remove_file(String.t(), Actor.t()) :: {:ok, Actor.t()}
