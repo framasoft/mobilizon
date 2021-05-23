0720c255cad0de5d9a29d40dcc32f85121ed52a1
Thomas Citharel <tcit@tcit.fr>
2021-05-23 18:48:44 +0200

    Remove useless call to config.prod.secret.exs
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- config/prod.exs
+++ config/prod.exs
@@ -43,9 +43,6 @@ cond do
       File.exists?("./config/#{System.get_env("INSTANCE_CONFIG")}") ->
     import_config System.get_env("INSTANCE_CONFIG")
 
-  File.exists?("./config/prod.secret.exs") ->
-    import_config "prod.secret.exs"
-
   true ->
     :ok
 end
