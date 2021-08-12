1280b66f419a92fc04be3e9e61be6c40d5ef08f6
Thomas Citharel <tcit@tcit.fr>
2021-08-12 10:11:46 +0200

    Catch blurhash decoding errors
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- js/src/components/Image/BlurhashImg.vue
+++ js/src/components/Image/BlurhashImg.vue
@@ -7,17 +7,21 @@ import { decode } from "blurhash";
 import { Component, Prop, Ref, Vue } from "vue-property-decorator";
 
 @Component
-export default class extends Vue {
+export default class BlurhashImg extends Vue {
   @Prop({ type: String, required: true }) hash!: string;
   @Prop({ type: Number, default: 1 }) aspectRatio!: string;
 
   @Ref("canvas") readonly canvas!: any;
 
   mounted(): void {
-    const pixels = decode(this.hash, 32, 32);
-    const imageData = new ImageData(pixels, 32, 32);
-    const context = this.canvas.getContext("2d");
-    context.putImageData(imageData, 0, 0);
+    try {
+      const pixels = decode(this.hash, 32, 32);
+      const imageData = new ImageData(pixels, 32, 32);
+      const context = this.canvas.getContext("2d");
+      context.putImageData(imageData, 0, 0);
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
 </script>
