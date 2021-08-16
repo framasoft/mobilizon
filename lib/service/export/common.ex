de102755f8878f2f73cfc3ff19c6a6f1da414add
Thomas Citharel <tcit@tcit.fr>
2021-08-16 16:15:52 +0200

    Add sane limits to feeds
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- lib/service/export/common.ex
+++ lib/service/export/common.ex
@@ -9,12 +9,12 @@ defmodule Mobilizon.Service.Export.Common do
   alias Mobilizon.Storage.Page
   alias Mobilizon.Users.User
 
-  @spec fetch_actor_event_feed(String.t()) :: String.t()
-  def fetch_actor_event_feed(name) do
+  @spec fetch_actor_event_feed(String.t(), integer()) :: String.t()
+  def fetch_actor_event_feed(name, limit) do
     with %Actor{} = actor <- Actors.get_actor_by_name(name),
          {:visibility, true} <- {:visibility, Actor.is_public_visibility?(actor)},
-         %Page{elements: events} <- Events.list_public_events_for_actor(actor),
-         %Page{elements: posts} <- Posts.get_public_posts_for_group(actor) do
+         %Page{elements: events} <- Events.list_public_events_for_actor(actor, 1, limit),
+         %Page{elements: posts} <- Posts.get_public_posts_for_group(actor, 1, limit) do
       {:ok, actor, events, posts}
     else
       err ->
@@ -23,8 +23,8 @@ defmodule Mobilizon.Service.Export.Common do
   end
 
   # Only events, not posts
-  @spec fetch_events_from_token(String.t()) :: String.t()
-  def fetch_events_from_token(token) do
+  @spec fetch_events_from_token(String.t(), integer()) :: String.t()
+  def fetch_events_from_token(token, limit) do
     with {:ok, uuid} <- ShortUUID.decode(token),
          {:ok, _uuid} <- Ecto.UUID.cast(uuid),
          %FeedToken{actor: actor, user: %User{} = user} <- Events.get_feed_token(uuid) do
@@ -33,7 +33,7 @@ defmodule Mobilizon.Service.Export.Common do
           %{
             type: :actor,
             actor: actor,
-            events: fetch_actor_private_events(actor),
+            events: fetch_actor_private_events(actor, limit),
             user: user,
             token: token
           }
@@ -42,7 +42,7 @@ defmodule Mobilizon.Service.Export.Common do
           with actors <- Users.get_actors_for_user(user),
                events <-
                  actors
-                 |> Enum.map(&fetch_actor_private_events/1)
+                 |> Enum.map(&fetch_actor_private_events(&1, limit))
                  |> Enum.concat() do
             %{type: :user, events: events, user: user, token: token, actor: nil}
           end
@@ -50,22 +50,22 @@ defmodule Mobilizon.Service.Export.Common do
     end
   end
 
-  @spec fetch_instance_public_content :: {:ok, list(Event.t()), list(Post.t())}
-  def fetch_instance_public_content do
-    with %Page{elements: events} <- Events.list_public_local_events(),
-         %Page{elements: posts} <- Posts.list_public_local_posts() do
+  @spec fetch_instance_public_content(integer()) :: {:ok, list(Event.t()), list(Post.t())}
+  def fetch_instance_public_content(limit) do
+    with %Page{elements: events} <- Events.list_public_local_events(1, limit),
+         %Page{elements: posts} <- Posts.list_public_local_posts(1, limit) do
       {:ok, events, posts}
     end
   end
 
-  @spec fetch_actor_private_events(Actor.t()) :: list(Event.t())
-  def fetch_actor_private_events(%Actor{} = actor) do
-    actor |> fetch_identity_participations() |> participations_to_events()
+  @spec fetch_actor_private_events(Actor.t(), integer()) :: list(Event.t())
+  def fetch_actor_private_events(%Actor{} = actor, limit) do
+    actor |> fetch_identity_participations(limit) |> participations_to_events()
   end
 
-  @spec fetch_identity_participations(Actor.t()) :: Page.t()
-  defp fetch_identity_participations(%Actor{} = actor) do
-    with %Page{} = page <- Events.list_event_participations_for_actor(actor) do
+  @spec fetch_identity_participations(Actor.t(), integer()) :: Page.t()
+  defp fetch_identity_participations(%Actor{} = actor, limit) do
+    with %Page{} = page <- Events.list_event_participations_for_actor(actor, 1, limit) do
       page
     end
   end
