3fcdba9c1fb65503b9665092b60c54392681e1df
Thomas Citharel <tcit@tcit.fr>
2021-09-30 08:31:52 +0200

    Add a config file for  vue-i18n-extract
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

---
+++ js/vue-i18n-extract.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  vueFiles: "./src/**/*.?(ts|js|vue)",
+  languageFiles: "./src/i18n/en_US.json",
+  output: false,
+  add: true,
+  remove: false,
+  ci: false,
+  separator: ".",
+};
