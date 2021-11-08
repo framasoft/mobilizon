41eb62bc2b7dd6a4c20d6f7f3740d7d8a605a66f
Thomas Citharel <tcit@tcit.fr>
2021-11-08 16:53:07 +0100

    Remove tzworld loading dynamically
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- lib/mobilizon.ex
+++ lib/mobilizon.ex
@@ -14,7 +14,6 @@ defmodule Mobilizon do
 
   import Cachex.Spec
 
-  alias Mix.Tasks.TzWorld.Update, as: TzWorldUpdate
   alias Mobilizon.{Config, Storage, Web}
   alias Mobilizon.Federation.ActivityPub
   alias Mobilizon.Service.{ErrorPage, ErrorReporting}
@@ -37,9 +36,6 @@ defmodule Mobilizon do
   @spec start(:normal | {:takeover, node} | {:failover, node}, term) ::
           {:ok, pid} | {:ok, pid, term} | {:error, term}
   def start(_type, _args) do
-    # We update TzWorld at runtime so that the data is not contained in releases
-    TzWorldUpdate.run(nil)
-
     children =
       [
         # supervisors
