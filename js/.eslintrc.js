a54607a57da2f985c81c6761b26d8d36a7e39bf8
Thomas Citharel <tcit@tcit.fr>
2021-11-09 08:53:04 +0100

    Upgrade deps
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -9,8 +9,7 @@ module.exports = {
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
 
   plugins: ["prettier"],
