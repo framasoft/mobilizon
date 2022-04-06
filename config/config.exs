bcf936d103bad2ec5385b79690725e494adfa10c
Thomas Citharel <tcit@tcit.fr>
2022-04-06 12:26:54 +0200

    Set initial analytics configuration
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- config/config.exs
+++ config/config.exs
@@ -345,6 +345,8 @@ config :mobilizon, :exports,
     Mobilizon.Service.Export.Participants.CSV
   ]
 
+config :mobilizon, :analytics, providers: []
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
