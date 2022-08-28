d2b59e6444b88d4c8e2437d8227fe863b129746c
Thomas Citharel <tcit@tcit.fr>
2022-08-28 10:54:24 +0200

    Add typings for vite import.meta.env
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- js/env.d.ts
+++ js/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="histoire/vue" />
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SERVER_URL: string;
+  readonly VITE_HISTOIRE_ENV: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
