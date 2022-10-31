be33c3b2138db8d1c388a054f32d95fcf8b37821
Thomas Citharel <tcit@tcit.fr>
2022-10-31 18:07:14 +0100

    Search improvements
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

---
+++ js/src/components/Group/SkeletonGroupResultList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-white dark:bg-slate-800 shadow rounded-md w-full mx-auto">
+    <div class="animate-pulse flex flex-col sm:flex-row space-3-4 items-center">
+      <div
+        class="object-cover h-40 w-40 rounded-full bg-slate-700 m-2 md:m-4 shrink-0"
+      />
+
+      <div
+        class="flex gap-3 flex self-start flex-col justify-between m-2 md:m-4 self-center w-full px-2 md:px-4"
+      >
+        <div class="h-5 bg-slate-700 w-64"></div>
+        <div class="h-3 bg-slate-700 w-52"></div>
+      </div>
+    </div>
+  </div>
+</template>
