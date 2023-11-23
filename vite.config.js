bfb7e3ca40d2ce71fdefc5fc07b3d21054f697b4
summersamara <summersamara@proton.me>
2023-11-23 15:33:31 +0100

    introduce VITE_HOST env var and pass it to the node watcher vite --host

--- vite.config.js
+++ vite.config.js
@@ -83,9 +83,6 @@ export default defineConfig(({ command }) => {
   return {
     plugins,
     build,
-    server: {
-      host: isDev ? "0.0.0.0" : "localhost",
-    },
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
