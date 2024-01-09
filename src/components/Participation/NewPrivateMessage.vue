ef20585f8cc1e4ac2f2f3359a70b7f456d2adeeb
Thomas Citharel <tcit@tcit.fr>
2024-01-09 15:49:03 +0100

    fix(announcements): error message not showing when an event announcement is created with empty text
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- src/components/Participation/NewPrivateMessage.vue
+++ src/components/Participation/NewPrivateMessage.vue
@@ -133,6 +133,7 @@ const sendForm = (e: Event) => {
   e.preventDefault();
   console.debug("Sending new private message");
   if (!currentActor.value?.id || !event.value.id) return;
+  errors.value = [];
   eventPrivateMessageMutate({
     text: text.value,
     actorId:
@@ -150,7 +151,10 @@ onEventPrivateMessageMutated(() => {
 
 onEventPrivateMessageError((err) => {
   err.graphQLErrors.forEach((error) => {
-    errors.value.push(error.message);
+    const message = Array.isArray(error.message)
+      ? error.message
+      : [error.message];
+    errors.value.push(...message);
   });
 });
 
